664261cbfe60332cf9f2161eb23d94238bab0a82
saurav <saurav32.ait@gmail.com>
2018-07-11 21:13:40 +0530

    port changes

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const axios = require("axios");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+const port = process.env.PORT || 8080;
+
 app.use(bodyParser.json());
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
@@ -21,26 +24,35 @@ app.use(function (req, res, next) {
 const API = "https://api.freebinchecker.com/bin";
 
 app.get("/bin", (req, res) => {
-  axios.get(`${API}/437551`).then((response) => {
-    res.status(200).send({ data: response.data })
-  }).catch((error) => {
-    res.status(500).send()
-  })
+  axios
+    .get(`${API}/437551`)
+    .then(response => {
+      res.status(200).send({ data: response.data });
+    })
+    .catch(error => {
+      res.status(500).send();
+    });
 });
-
 
 app.get("/bin/:firstSixDigit", (req, res) => {
   if (req.params.firstSixDigit) {
-    axios.get(`${API}/${req.params.firstSixDigit}`).then((response) => {
-      res.status(200).send({ data: response.data })
-    }).catch((error) => {
-      res.status(500).send()
-    })
+    axios
+      .get(`${API}/${req.params.firstSixDigit}`)
+      .then(response => {
+        res.status(200).send({ data: response.data });
+      })
+      .catch(error => {
+        res.status(500).send();
+      });
   } else {
-    res.status(400).send()
+    res.status(400).send();
   }
 });
 
-app.listen("8080", () => {
+app.get("*", (req, res) => {
+  res.send({ name: "Saurav" });
+});
+
+app.listen(port, () => {
   console.log("Server started");
 });
