7ac27bf0b9889109201dc30133a5569ffcd069f9
saurav <saurav32.ait@gmail.com>
2018-07-11 21:02:35 +0530

    initial code

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, GET, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+const API = "https://api.freebinchecker.com/bin";
+
+app.get("/bin", (req, res) => {
+  axios.get(`${API}/437551`).then((response) => {
+    res.status(200).send({ data: response.data })
+  }).catch((error) => {
+    res.status(500).send()
+  })
+});
+
+
+app.get("/bin/:firstSixDigit", (req, res) => {
+  if (req.params.firstSixDigit) {
+    axios.get(`${API}/${req.params.firstSixDigit}`).then((response) => {
+      res.status(200).send({ data: response.data })
+    }).catch((error) => {
+      res.status(500).send()
+    })
+  } else {
+    res.status(400).send()
+  }
+});
+
+app.listen("8080", () => {
+  console.log("Server started");
+});
